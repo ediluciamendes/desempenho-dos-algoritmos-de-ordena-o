c95caa21a70447e0f6597a9ec1db277880b7a30e
ediluciamendes <129550897+ediluciamendes@users.noreply.github.com>
2024-03-07 16:34:41 -0300

    códigos

---
+++ bubble.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Função para ordenar usando Bubble Sort
+void bubble_sort(int arr[], int n, int *comp, int *trocas) {
+    int i, j;
+    *comp = 0;
+    *trocas = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            (*comp)++;
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                (*trocas)++;
+            }
+        }
+    }
+}
+
+// Função para gerar vetor aleatório
+void vetor_aleatorio(int arr[], int size) {
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < size; i++) {
+        arr[i] = rand() % 1000000 + 1;
+    }
+}
+
+// Função para medir o desempenho
+void desempenho(void (*sort_func)(int[], int, int*, int*), int arr[], int size, int *comp, int *trocas, double *tempo_execucao) {
+    clock_t inicio, fim;
+    inicio = clock();
+    (*sort_func)(arr, size, comp, trocas);
+    fim = clock();
+    *tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+}
+
+int main() {
+    int vector_size = 10000;
+    int aleatorio_arr[vector_size];
+    int ordenado_arr[vector_size];
+    int inverso_arr[vector_size];
+    int comp, trocas;
+    double tempo_execucao;
+
+    vetor_aleatorio(aleatorio_arr, vector_size);
+
+    for (int i = 0; i < vector_size; i++) {
+        ordenado_arr[i] = i + 1;
+    }
+
+    for (int i = 0; i < vector_size; i++) {
+        inverso_arr[i] = vector_size - i;
+    }
+
+    // Medição de desempenho para Bubble Sort
+    desempenho(bubble_sort, aleatorio_arr, vector_size, &comp, &trocas, &tempo_execucao);
+    printf("Bubble Sort\n");
+    printf("Comparações: %d\n", comp);
+    printf("Trocas: %d\n", trocas);
+    printf("Tempo de execução: %.6f segundos\n", tempo_execucao);
+
+    return 0;
+}
